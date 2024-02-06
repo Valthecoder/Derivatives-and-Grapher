00e4eb67858705873a66e2894ef6bad340df9b8e
ValtheCoder <keshia.kdf@gmail.com>
2024-02-06 22:45:19 +0800

    Tried but I think I can't

--- ValenciaFinalProj.py
+++ ValenciaFinalProj.py
@@ -13,3 +13,22 @@ def f(x): return 5*x**3 - 3*x
 x = sy.Symbol('x')
 der = sy.diff(f(x), x)
 print(der)
+# Try to x² not x**2
+print("x\u00b2 + y\u00b2 = 2")  # x² + y² = 2
+# Think of a nice and cool program in the Google
+# Try Derivatives and Graph Plotting
+# Credits the source
+# Create some adjustments or new in the program
+# Done
+# Imports
+import sympy as sy
+
+
+def f(x): return 5*x**3 - 3*x
+
+
+x = sy.Symbol('x')
+der = sy.diff(f(x), x)
+print(der)
+# Try to x² not x**2
+print("x\u00b2 + y\u00b2 = 2")  # x² + y² = 2
