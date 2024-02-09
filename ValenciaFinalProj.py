ddc448202ace71b4fe8fd33a1468e5d07541eb99
ValtheCoder <keshia.kdf@gmail.com>
2024-02-09 18:53:16 +0800

    Input working need some reminders for input

--- ValenciaFinalProj.py
+++ ValenciaFinalProj.py
@@ -4,23 +4,26 @@
 # Create some adjustments or new in the program
 # Done
 # Imports
+import sympy.core.add
 from sympy import *
-import sympy as smp
+from scipy.misc import derivative
+import sympy as sym
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.misc import derivative
-x, a, b, c = smp.symbols('x a b c', real=True)
-func_y = input("Enter a Function: \n")
+from fractions import Fraction
 
-print(func_y)
+x = Symbol('x')
+y = input('Enter a Function (x² = x**2 and 2x = 2*x) : \n y = ')
+func_y = sympify(y)
+yprime = func_y.diff(x)
+print("y' = ", yprime)
 
-dydx = smp.diff(func_y, x)
-print("dy/dx or y'=", dydx)
-dydx.subs([(x, 4), (a, 1), (b, 2), (c, 3)]).evalf()
-dydx_f = smp.lambdify((x, a, b, c), dydx)
+x, a, b, c = sym.symbols('x a b c', real=True)
+yprime.subs([(x, 4), (a, 1), (b, 2), (c, 3)]).evalf()
+yprime_f = sym.lambdify((x, a, b, c), yprime)
 x = np.linspace(1, 2, 100)
-y = dydx_f(x, a=1, b=2, c=3)
+y = yprime_f(x, a=1, b=2, c=3)
 plt.plot(x, y)
 plt.ylabel('$dy / dx$', fontsize=24)
 plt.xlabel('$x$', fontsize=24)
